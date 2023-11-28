c4a695ad53486ef1cb0a3a0f62fd2f08c3575a22
wideraHannes <61525214+wideraHannes@users.noreply.github.com>
2023-11-28 14:50:15 +0100

    gave labels page 6-8 more space

--- src/pdf/services/create-charts/chart-utility.service.ts
+++ src/pdf/services/create-charts/chart-utility.service.ts
@@ -41,7 +41,7 @@ export class ChartUtilityService {
       scales: {
         y: {
           afterFit: function (scaleInstance) {
-            scaleInstance.width = 430; // sets the width to 100px
+            scaleInstance.width = 500; // Sets width of Labels
           },
           ticks: {
             font: {
