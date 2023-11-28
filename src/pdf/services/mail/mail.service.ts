f7640e0bba51199711f7b72a09528ff2df253d92
wideraHannes <61525214+wideraHannes@users.noreply.github.com>
2023-11-28 18:08:23 +0100

    compress pdf

--- src/pdf/services/mail/mail.service.ts
+++ src/pdf/services/mail/mail.service.ts
@@ -1,7 +1,9 @@
-import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 import * as AWS from 'aws-sdk';
+const AdmZip = require('adm-zip');
+const fs = require('fs');
+import * as zlib from 'zlib';
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -21,24 +23,43 @@ export class MailService {
   }
 
   async sendMail(email: string, report: string) {
-    const mailOptions = {
-      from: 'no-reply@newcareer.ch',
-      to: email,
-      subject: 'Your Report',
-      text: 'Here is your report',
-      attachments: [
-        {
-          filename: 'report.pdf',
-          path: report,
-        },
-      ],
-    };
-
     try {
+      const pdfStream = fs.createReadStream(report);
+      const compressedStream = pdfStream.pipe(zlib.createGzip());
+
+      // Convert the compressed stream to a buffer
+      const compressedData = await new Promise((resolve, reject) => {
+        const chunks = [];
+        compressedStream.on('data', (chunk) => chunks.push(chunk));
+        compressedStream.on('end', () => resolve(Buffer.concat(chunks)));
+        compressedStream.on('error', reject);
+      });
+
+      const REPORT_FILENAME = 'report.pdf.gz';
+      const ZIP_FILENAME = 'report.zip';
+      const zip = new AdmZip();
+      zip.addFile(REPORT_FILENAME, compressedData, '', 0o644);
+      const zipBuffer = zip.toBuffer();
+
+      const EMAIL_FROM = 'no-reply@newcareer.ch';
+      const mailOptions = {
+        from: EMAIL_FROM,
+        to: email,
+        subject: 'Your Report',
+        text: 'Here is your report',
+        attachments: [
+          {
+            filename: ZIP_FILENAME,
+            content: zipBuffer, // SES max size is 10MB, so we need to zip the file
+          },
+        ],
+      };
+
       const info = await this.transporter.sendMail(mailOptions);
       console.log('Email sent successfully: ', info.response);
     } catch (error) {
       console.error('Unable to send mail: ', error);
+      throw error;
     }
   }
 }
