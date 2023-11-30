e2e92e9a9ae75124ae1170830c0e4e9d2e7a5164
wideraHannes <61525214+wideraHannes@users.noreply.github.com>
2023-11-30 12:43:00 +0100

    added new E4F index code

--- src/pdf/services/build-pdf/pages/page6.service.ts
+++ src/pdf/services/build-pdf/pages/page6.service.ts
@@ -5,6 +5,9 @@ import { ChartUtilityService } from '../../create-charts/chart-utility.service';
 
 @Injectable()
 export class Page6Service {
+  readonly KLIENT_COLOR = '#382d73';
+  readonly STAKEHOLDER_COLOR = '#d5453c';
+
   constructor(private readonly chartUtilityService: ChartUtilityService) {}
   async addContentToPage(
     doc: PDFDocument,
@@ -16,12 +19,6 @@ export class Page6Service {
       width: 620,
       height: 842,
     });
-
-    const barChart = await this.chartUtilityService.createBarChartE4FIndex(
-      klientMap,
-      stakeholderMap,
-    );
-    doc.image(barChart, 50, 638, { width: 425, height: 60 });
 
     this.enrichMapWithPercentage(klientMap);
     this.enrichMapWithPercentage(stakeholderMap);
@@ -43,7 +40,41 @@ export class Page6Service {
     const doughnut = await this.createDoughnutCharts(klientMap);
     this.addDoughnutCharts(doc, doughnut, klientMap);
 
+    /*     const barChart = await this.chartUtilityService.createBarChartE4FIndex(
+      klientMap,
+      stakeholderMap,
+    );
+    doc.image(barChart, 50, 638, { width: 425, height: 60 }); */
+
+    this.createBarChartE4FIndex(doc, klientMap, stakeholderMap);
+
     return;
+  }
+
+  private createBarChartE4FIndex(doc, klientMap, stakeholderMap) {
+    const klientAverage = this.calculateAverageOfAverages(klientMap);
+    const stakeholderAverage = this.calculateAverageOfAverages(stakeholderMap);
+    doc.lineWidth(20);
+    const X_POS_LABEL = 260;
+
+    // line cap settings (x, y)
+    doc
+      .lineCap('butt')
+      .moveTo(63, 632)
+      .lineTo(300, 632)
+      .fillAndStroke(this.STAKEHOLDER_COLOR, this.STAKEHOLDER_COLOR);
+
+    doc
+      .lineCap('butt')
+      .moveTo(63, 665)
+      .lineTo(466, 665)
+      .fillAndStroke(this.KLIENT_COLOR, this.KLIENT_COLOR);
+
+    doc.fillColor('#ffffff');
+    doc.fontSize(6);
+    doc.font('Helvetica-Bold');
+    doc.text('Stakeholder', X_POS_LABEL, 630);
+    doc.text('Klient', X_POS_LABEL + 15, 663);
   }
 
   private addDoughnutCharts(
@@ -143,5 +174,18 @@ export class Page6Service {
         (competenceData.averageRating / 0.05).toFixed(2),
       );
     }
+  }
+
+  private calculateAverageOfAverages(data: CompetenceData) {
+    let totalAverage = 0;
+    let numCompetences = 0;
+
+    for (const competenceData of Object.values(data)) {
+      totalAverage += competenceData.averageRating;
+      numCompetences++;
+    }
+
+    const averageOfAverages = totalAverage / numCompetences;
+    return averageOfAverages;
   }
 }
